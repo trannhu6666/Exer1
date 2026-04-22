1b2ee91774f2610b4ba07b69b1572ad6277fe61d
trannhu6666 <trannhu6666@WIN-V5RTT9VD5R7>
2026-04-22 08:10:30 -0700

    add test

---
+++ SaleManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SaleManagement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dockur")]
+[assembly: AssemblyProduct("SaleManagement")]
+[assembly: AssemblyCopyright("Copyright © Dockur 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08648d7a-4aaf-4db0-ba6c-66d6487c8465")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SaleManagement/RepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using System.Linq;
+
+namespace SaleManagement.Tests
+{
+    [TestClass]
+    public class UserRepositoryTests
+    {
+        private UserRepository _userRepo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _userRepo = new UserRepository();
+        }
+
+        [TestMethod]
+        public void Login_ValidUser_ReturnsTrue()
+        {
+            bool result = _userRepo.Login("marketing@gmail.com", "123");
+            Assert.IsTrue(result, "Đăng nhập hợp lệ phải trả về True.");
+        }
+
+        [TestMethod]
+        public void Login_InvalidUser_ReturnsFalse()
+        {
+            bool result = _userRepo.Login("sai_email@gmail.com", "sai_pass");
+            Assert.IsFalse(result, "Đăng nhập sai phải trả về False.");
+        }
+    }
+
+    [TestClass]
+    public class AgentRepositoryTests
+    {
+        private AgentRepository _agentRepo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _agentRepo = new AgentRepository();
+        }
+
+        [TestMethod]
+        public void AddAgent_ValidAgent_IncreasesTotalAgents()
+        {
+            int initialCount = _agentRepo.GetAllAgents().Count;
+
+            var newAgent = new Agent { AgentName = "Test Agent", Address = "123 Street" };
+            _agentRepo.AddAgent(newAgent);
+
+            int newCount = _agentRepo.GetAllAgents().Count;
+            Assert.AreEqual(initialCount + 1, newCount, "Số lượng đại lý phải tăng lên 1.");
+        }
+    }
+}
